401520a47db443dd362b10c1a319e65c9b663fc6
nkrishang <kn1457@nyu.edu>
2021-08-10 14:41:43 -0400

    Added handleNativeRewards

--- src/reward.ts
+++ src/reward.ts
@@ -1,84 +1,63 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  Pack,
-  ApprovalForAll,
-  PackCreated,
-  PackOpenFulfilled,
-  PackOpenRequest,
-  TransferBatch,
-  TransferSingle,
-  URI
-} from "../generated/Pack/Pack"
-import { ExampleEntity } from "../generated/schema"
+  NativeRewards,
+} from "../generated/Reward/Reward"
 
-export function handleApprovalForAll(event: ApprovalForAll): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+import {
+  Account, RewardOwnership, Reward, RewardUnderlyingToken
+} from "../generated/schema"
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+const rewardContractAddress: string = "0xF0D1064ec8Dee772af45D6e9E45Cfa5F429d80a7"
+
+/**
+ * 
+ * @param event NativeRewards(address indexed creator, uint[] rewardIds, string[] rewardURIs, uint[] rewardSupplies)
+ */
+export function handleNativeRewards(event: NativeRewards): void {
+
+  // Update `Account` for creator
+  let creatorAccountId: string = event.params.creator.toHex()
+  let creatorAccount = Account.load(creatorAccountId)
+
+  if(creatorAccount == null) {
+    creatorAccount = new Account(creatorAccountId)
+    creatorAccount.save()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let rewardIds = event.params.rewardIds
+  let rewardURIs = event.params.rewardURIs
+  let rewardSupplies = event.params.rewardSupplies
 
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.operator = event.params.operator
+  for(let i = 0; i < rewardIds.length; i++) {
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    // Create `Reward`
+    let rewardId = rewardIds[i].toString()
+    let reward = new Reward(rewardId)
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+    reward.tokenId = rewardIds[i]
+    reward.creator = creatorAccountId
+    reward.uri = rewardURIs[i]
+    reward.supply = rewardSupplies[i]
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balanceOf(...)
-  // - contract.balanceOfBatch(...)
-  // - contract.createPack(...)
-  // - contract.creator(...)
-  // - contract.getPack(...)
-  // - contract.getPackWithRewards(...)
-  // - contract.getRewardsInPack(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.nextTokenId(...)
-  // - contract.onERC1155BatchReceived(...)
-  // - contract.onERC1155Received(...)
-  // - contract.packs(...)
-  // - contract.pendingRequests(...)
-  // - contract.randomnessRequests(...)
-  // - contract.rewards(...)
-  // - contract.supportsInterface(...)
-  // - contract.uri(...)
-  // - contract.vrfFees(...)
-  // - contract.vrfKeyHash(...)
+    reward.save()
+
+    // Create `RewardUnderlyingToken`
+    let rewardUnderlyingTokenId = rewardContractAddress + rewardId + '-underlying'
+    let rewardUnderlyingToken = new RewardUnderlyingToken(rewardUnderlyingTokenId)
+
+    rewardUnderlyingToken.type = "None"
+    rewardUnderlyingToken.reward = rewardId
+
+    rewardUnderlyingToken.save()
+
+    // Create `RewardOwnership` for creator
+    let rewardOwnershipId = creatorAccountId + rewardContractAddress + rewardId
+    let rewardOwnership = new RewardOwnership(rewardOwnershipId)
+
+    rewardOwnership.owner = creatorAccountId
+    rewardOwnership.balance = rewardSupplies[i]
+    rewardOwnership.reward = rewardId
+
+    rewardOwnership.save()
+  }
 }
-
-export function handlePackCreated(event: PackCreated): void {}
-
-export function handlePackOpenFulfilled(event: PackOpenFulfilled): void {}
-
-export function handlePackOpenRequest(event: PackOpenRequest): void {}
-
-export function handleTransferBatch(event: TransferBatch): void {}
-
-export function handleTransferSingle(event: TransferSingle): void {}
-
-export function handleURI(event: URI): void {}
